d58023d92218a1f0eced4252618c9e1ba82ca17c
Ashley Burnett <82122780+ashleyb6@users.noreply.github.com>
2022-02-20 20:29:59 -1000

    Update record.h

--- record.h
+++ record.h
@@ -1,22 +1,3 @@
-/*****************************************************************
-//
-//  NAME:        Ashley Burnett
-//
-//  HOMEWORK:    Project1
-//
-//  CLASS:       ICS 212
-//
-//  INSTRUCTOR:  Ravi Narayan
-//
-//  DATE:        February 10, 2021
-//
-//  FILE:        record.h
-//
-//  DESCRIPTION:
-//   Definition of new datatype, record, and its properties.
-//
-****************************************************************/
-
 struct record
 {
    int                accountno;
